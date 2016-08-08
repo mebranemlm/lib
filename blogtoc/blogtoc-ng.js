70f9ca7909b57fa454a36b3cbfe21c4e953cc974
aedi <none>
2016-08-08 05:34:17 -0500

    order by updated

--- blogtoc/blogtoc-ng.js
+++ blogtoc/blogtoc-ng.js
@@ -26,17 +26,16 @@ angular.module('blogtoc', [])
     
   // }
 }])
-// .filter('filterByTag', function() {
-//     return function(posts, tag) {
-//         return posts.filter(function(post) {
-//             var sw=false;
-//             post.category.map(function(cate,i){
-//                         if(cate.term==tag){
-//                           sw=true;
-//                         }
-//               });
-//             return sw;
-
-//         });
-//     };
-// })
+.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});
