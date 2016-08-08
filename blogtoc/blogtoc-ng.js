1b803a881cd77d0b9fcf4011e88fe42924dd2d7f
aedi <none>
2016-08-08 05:08:27 -0500

    filter by tag

--- blogtoc/blogtoc-ng.js
+++ blogtoc/blogtoc-ng.js
@@ -1,9 +1,40 @@
 angular.module('blogtoc', [])
 .controller('btcontroller', ['$scope', function($scope){
   // window.onload=function(){
-    
+      $scope.filtro={};
       $scope.posts=json.feed.entry;
       //console.log($scope.posts.length);
+      
+      $scope.filterByTag=function(tag){
+        // var aux={
+        //  // scheme:tag.scheme,
+        //   term:tag.term
+        // }
+        $scope.filtro={ 
+          category: {
+            term:tag.term
+          }
+        }
+       // console.log(aux);
+      }
+
+      $scope.showAll=function(){
+        $scope.filtro={}
+      }
     
   // }
 }])
+// .filter('filterByTag', function() {
+//     return function(posts, tag) {
+//         return posts.filter(function(post) {
+//             var sw=false;
+//             post.category.map(function(cate,i){
+//                         if(cate.term==tag){
+//                           sw=true;
+//                         }
+//               });
+//             return sw;
+
+//         });
+//     };
+// })
