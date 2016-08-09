9e3c896adfb5fcc24a6dc0f8c83083d7d61390b5
aedi <none>
2016-08-09 02:00:42 -0500

    fixing day

--- bloggerPaginator/blogger-page-nav-v2.1.js
+++ bloggerPaginator/blogger-page-nav-v2.1.js
@@ -24,7 +24,7 @@ function showpageCount(json) {
     	// 	dpost=new Date(dpost);
     	// }
     	var mes=`0${dpost.getMonth()+1}`;
-    	var dia=`0${dpost.getDay()}`;
+    	var dia=`0${dpost.getDate()}`;
     	//var dia=`0${dpost.getDay()}`;
     	var hora=`0${dpost.getHours()}`;
     	var min=`0${dpost.getMinutes()}`;
