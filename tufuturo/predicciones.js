c33a320acb1830d05290939236dd6523f7f85e7d
LennonMb <makinaface@gmail.com>
2019-09-21 13:03:37 -0500

    Update predicciones.js

--- tufuturo/predicciones.js
+++ tufuturo/predicciones.js
@@ -1,8 +1,10 @@
 var lista_predicciones=[
-`Trabajo y negocios:
-Superarás las dificultades y los resultados comenzarán a ser mejor de lo esperado. 
 
-Amor:
-Tu rasgo atrevido seducirá a la persona menos imaginada; tendrás un romance inolvidable.`
+`
+No deberás preocuparte por un problema que al menos de momento no tiene solución, aunque la paciencia será una gran virtud que te permitirá resurgir, será conveniente que te enfoques en otras propuestas sin perder detalle del progreso. Algunas diferencias con una persona cercana harán que sea una época complicada para los compromisos, esto te mantendrá alejado de tu entorno. En el amor los matices te interesarán, sobre todo con las amistades, por eso hoy estarás muy pendiente de las sensaciones con los demás. Será un día propicio para quedar con esa persona con la que has estado hablando por WhatsApp, el universo propiciará el encuentro, aunque no habrá una gran conexión al instante será conveniente no te fíes de la primera impresión. Buena salud para este mes, tendrás mucha energía. `,
 
+`
+Estás a punto de descubrir una faceta para tu futuro que te abrirá muchas puertas a nuevos terrenos. Estarás en una etapa de tu vida en la que querrás romper con esa parte que no te gusta, será un proceso largo, pero estarás decidido|decidida hacia dónde quieres dirigirte, aprenderás de manera educada a expresar mejor tus opiniones sin complicarte tu existencia. En el amor sentirás que las cosas están cambiando, será conveniente que hoy afrontes la situación y hables con esa persona antes que las cosas cambien solas. El universo te permitirá que seas sincero|sincera contigo mismo|misma y con esa persona especial. Habrá muchos cambios, aunque tendrás buena salud deberás ser cauto|cauta con los excesos. `,  
+
+  
 ]
