65f30e6012d4564b935fbf24fd290ab4b7094c9e
LennonMb <makinaface@gmail.com>
2019-09-21 12:48:32 -0500

    Create predicciones.js

---
+++ tufuturo/predicciones.js
@@ -0,0 +1,8 @@
+var lista_predicciones=[
+`Trabajo y negocios:
+Superarás las dificultades y los resultados comenzarán a ser mejor de lo esperado. 
+
+Amor:
+Tu rasgo atrevido seducirá a la persona menos imaginada; tendrás un romance inolvidable.`
+
+]
