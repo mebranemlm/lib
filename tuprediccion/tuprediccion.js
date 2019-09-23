fcd9b74a3cc5eae6e2679004e694877379349f48
LennonMb <makinaface@gmail.com>
2019-09-23 13:50:08 -0500

    Create tuprediccion.js

---
+++ tuprediccion/tuprediccion.js
@@ -0,0 +1,47 @@
+var lista_predicciones=[
+`
+No deberás preocuparte por un problema que al menos de momento no tiene solución, aunque la paciencia será una gran virtud que te permitirá resurgir, será conveniente que te enfoques en otras propuestas sin perder detalle del progreso. Algunas diferencias con una persona cercana harán que sea una época complicada para los compromisos, esto te mantendrá alejado de tu entorno. En el amor los matices te interesarán, sobre todo con las amistades, por eso hoy estarás muy pendiente de las sensaciones con los demás. Será un día propicio para quedar con esa persona con la que has estado hablando por WhatsApp, el universo propiciará el encuentro, aunque no habrá una gran conexión al instante será conveniente no te fíes de la primera impresión. Buena salud para este mes, tendrás mucha energía. 
+`,
+ 
+`
+Estás a punto de descubrir una faceta para tu futuro que te abrirá muchas puertas a nuevos terrenos. Estarás en una etapa de tu vida en la que querrás romper con esa parte que no te gusta, será un proceso largo, pero estarás decidido|decidida hacia dónde quieres dirigirte, aprenderás de manera educada a expresar mejor tus opiniones sin complicarte tu existencia. En el amor sentirás que las cosas están cambiando, será conveniente que hoy afrontes la situación y hables con esa persona antes que las cosas cambien solas. El universo te permitirá que seas sincero|sincera contigo mismo|misma y con esa persona especial. Habrá muchos cambios, aunque tendrás buena salud deberás ser cauto|cauta con los excesos. 
+`,  
+
+`
+El dinero que tanto necesitas llegará de manera inesperada, aunque deberá abrirse un camino que hasta ahora había mantenido cerrado, será un proceso que requerirá de mucha paciencia, pero la espera valdrá la pena, si sigues tu intuición el éxito llegará en este mismo año. Los planes de este mes te despertarán ciertas inquietudes personales que podrás resolver si miras bien a tu alrededor. En el amor, entre hoy y mañana buscarás a esa persona con quien planearás alguna salida de fin de semana. Además harás un viaje con los amigos donde conocerás a una persona que te hará sentir cosas totalmente diferentes a lo que concebías. Tendrás mucha energía que se verá reflejada en tu buena salud. 
+`,  
+
+`
+Se acercan nuevas oportunidades para ti, por lo que debes estar atento|atenta a las cosas que pasan a tu alrededor a partir de hoy, ya que te confiarán un proyecto que cambiará por completo tu vida y te permitirá desarrollarte. Estarás en una etapa en la que te interesará explorar cosas nuevas, por ello, probarás nuevas experiencias que te abrirán muchas expectativas y posibilidades de crecimiento personal.  Mantendrás una actitud bastante atractiva que te permitirá encontrar el amor que buscas o fortalecer tu relación de pareja. Deberás cuidarte del clima ya que afectará tu salud. 
+`,   
+]
+
+var lista_frases=[
+`El mundo está lleno de magia que espera pacientemente a que nuestro ingenio se afine.`,
+`Las cosas más nobles y bellas de la vida no están hechas para que escuchemos historias sobre ellas, para leamos acerca de su existencia o para que las veamos, sino que son para ser vividas.`,
+`La felicidad no es la ausencia de problemas sino la capacidad de lidiar con ellos.`,
+`No importa a quién conozcas a lo largo de tu vida, tomarás algo de ellos, ya sea positivo o negativo.`,
+`Tener una actitud positiva es preguntarse cómo puede hacerse algo, más que decir que no puede hacerse.`,
+`Estoy agradecido a todos los que me dijeron no. Gracias a ellos lo estoy haciendo por mí mismo.`,
+`La mejor manera de olvidar las malas cosas de la vida es aprender a recordar las cosas buenas`,
+`La mejor manera de predecir el futuro es creándolo.`,
+`Todos nuestros sueños se pueden volver realidad si tenemos la valentía de perseguirlos.`,
+`El fracaso es una buena oportunidad para empezar de nuevo con más inteligencia.`,
+`Lo que no te mata te hace más fuerte.`,
+`Si exagerásemos nuestras alegrías, como hacemos con nuestras penas, nuestros problemas perderían importancia.`,
+`Cada día me miro en el espejo y me pregunto: "Si hoy fuese el último día de mi vida, &#191;querría hacer lo que voy a hacer hoy?". Si la respuesta es "No" durante demasiados días consecutivos, sé que debo cambiar algo.`,
+`Los únicos interesados en cambiar el mundo son los pesimistas, porque los optimistas están encantados con lo que hay.`,
+`Incluso la noche más oscura terminará con la salida del Sol.`,
+`Nunca eres demasiado viejo para tener otra meta u otro sueño.`,
+`Nadie que haya dado lo mejor de sí mismo lo ha lamentado`,
+`La gente positiva cambia el mundo, mientras que la negativa lo mantiene como está`,
+`Desechad tristezas y melancolías. La vida es amable, tiene pocos días y tan sólo ahora la hemos de gozar.`,
+`Vale más un diamante con defecto que un pedrusco sin ninguno.`,
+`Es una locura amar, a menos que se ame con locura`,
+`Las oportunidades y las ventajas están detrás de cada puerta que abro.`,
+`Estar en la ruina es una situación temporal. Ser pobre es un estado mental.`,
+`Me estoy volviendo mejor en lo que hago todos los días.`,
+`A cada paso, una oportunidad aparece frente a mí.`,
+`La salud no es sólo la ausencia de enfermedad, sino también la armonía con uno mismo y el entorno.`,
+
+]
