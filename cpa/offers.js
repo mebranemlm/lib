ec8e1aa0a996946a93ab236d256d47b6d8c7233c
LennonMb <makinaface@gmail.com>
2019-09-23 19:51:28 -0500

    Update offers.js

--- cpa/offers.js
+++ cpa/offers.js
@@ -3,7 +3,7 @@ var cpa_offers = [
         name: "Completa una encuesta completa para obtener tu recompensa!",
         titles: ["Completa una encuesta para obtener tu recompensa!"],
         imgs: ["https://i.imgur.com/PhX1ekv.jpg", "https://imgur.com/axbPXP0", "https://i.imgur.com/bxcUBCa.png"],
-        countries: ["MX"],
+        countries: ["PE","MX"],
         url: "http://ito.mx/LCSM",
         web: "CPALead",
         user: "liber...",
