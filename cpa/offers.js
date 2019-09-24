3f767d9008cc97f6e0c83ae43884780c5fd25eba
LennonMb <makinaface@gmail.com>
2019-09-23 19:45:03 -0500

    Update offers.js

--- cpa/offers.js
+++ cpa/offers.js
@@ -1,6 +1,6 @@
 var offers = [
     {
-        titles: ["Completa una encuesta completa para obtener tu recompensa!"],
+        titles: ["Completa una encuesta para obtener tu recompensa!"],
         imgs: ["https://i.imgur.com/PhX1ekv.jpg", "https://imgur.com/axbPXP0", "https://i.imgur.com/bxcUBCa.png"],
         countries: ["MX"],
         url: "http://ito.mx/LCSM",
