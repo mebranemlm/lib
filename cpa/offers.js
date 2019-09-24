82c9c93c67ffa528f8a120646c5e215bb43c14b6
LennonMb <makinaface@gmail.com>
2019-09-23 19:45:39 -0500

    Update offers.js

--- cpa/offers.js
+++ cpa/offers.js
@@ -1,5 +1,6 @@
 var offers = [
     {
+        name: "Completa una encuesta completa para obtener tu recompensa!",
         titles: ["Completa una encuesta para obtener tu recompensa!"],
         imgs: ["https://i.imgur.com/PhX1ekv.jpg", "https://imgur.com/axbPXP0", "https://i.imgur.com/bxcUBCa.png"],
         countries: ["MX"],
