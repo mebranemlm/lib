cbf7c391922971fa988ae6f653e3c3cd4ce45a3f
LennonMb <makinaface@gmail.com>
2019-09-23 19:44:46 -0500

    Update offers.js

--- cpa/offers.js
+++ cpa/offers.js
@@ -1,13 +1,12 @@
-var offers=[
-{
-  titles:["Completa una encuesta completa para obtener tu recompensa!"],
-  imgs:["https://i.imgur.com/PhX1ekv.jpg","https://imgur.com/axbPXP0","https://i.imgur.com/bxcUBCa.png"],
-countries:["MX"],
-url:"http://ito.mx/LCSM",
-web:"CPALead",
-user:"liber...",
-status:"A",
-type:"email"
-}
-
-]
+var offers = [
+    {
+        titles: ["Completa una encuesta completa para obtener tu recompensa!"],
+        imgs: ["https://i.imgur.com/PhX1ekv.jpg", "https://imgur.com/axbPXP0", "https://i.imgur.com/bxcUBCa.png"],
+        countries: ["MX"],
+        url: "http://ito.mx/LCSM",
+        web: "CPALead",
+        user: "liber...",
+        status: "A",
+        type: "email"
+    }
+];
